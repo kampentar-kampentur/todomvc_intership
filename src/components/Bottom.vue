0bb84bbf51c353c220a58ba4233625317d0a7869
koksss03 <koksss03@gmail.com>
2019-07-10 18:23:21 +0300

    last comit

--- src/components/Bottom.vue
+++ src/components/Bottom.vue
@@ -4,14 +4,8 @@
       <strong>{{counter}}</strong> items left
     </span>
     <ul class="filters">
-      <li>
-        <router-link :to="{ name: 'home', params: { filter: '' } }">All</router-link>
-      </li>
-      <li>
-        <router-link :to="{ name: 'home', params: { filter: 'active' } }">Active</router-link>
-      </li>
-      <li>
-        <router-link :to="{ name: 'home', params: { filter: 'completed' } }">Completed</router-link>
+      <li v-for="filterParam in filterParams" :key="filterParam.to.params.filter">
+        <router-link :to="filterParam.to">{{ filterParam.title }}</router-link>
       </li>
     </ul>
     <button class="clear-completed" @click="clearCompleated">Clear completed</button>
@@ -28,7 +22,22 @@ export default {
     }
   },
   data() {
-    return {}
+    return {
+      filterParams: [
+        {
+          to: { name: 'home', params: { filter: '' } },
+          title: 'All'
+        },
+        {
+          to: { name: 'home', params: { filter: 'active' } },
+          title: 'Active'
+        },
+        {
+          to: { name: 'home', params: { filter: 'completed' } },
+          title: 'Completed'
+        }
+      ]
+    }
   },
   methods: {
     clearCompleated() {
